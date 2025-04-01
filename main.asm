108c0fb11588119dfe404a5340fd2023907614a6
PolyVectors <PolyVector@protonmail.com>
2025-04-01 21:35:14 +0100

    better errors

--- main.asm
+++ main.asm
@@ -1,10 +1,10 @@
-format ELF64 executable
+format ELF64 executable 3
+entry _start
 
 include 'inc/fmt.inc'
 include 'inc/err.inc'
 include 'inc/str.inc'
 
-entry _start
 _start:
 	pop [argc]
 	add rsp, 8
@@ -12,31 +12,51 @@ _start:
 	
 	cmp QWORD [argc], 1
 	je no_arguments
+	cmp QWORD [argc], 4
+	jge too_many_arguments
 
 	mov r15, [argv]
-	mov r14, help_short
+	mov r14, _help_short
 	call strcmp
 	cmp r13, 0
 	je help
 
-	mov r14, help_long
+	mov r14, _help_long
 	call strcmp
 	cmp r13, 0
 	je help
+
+	mov rax, 2
+	mov rdi, [argv]
+	mov rsi, O_RDONLY
+	mov rdx, 0444 ; read only? (might be doing this wrong)
+	syscall
+
+	cmp rax, 0
+	jl no_file
+
+	mov rdi, rax
+	mov rax, 3
+	syscall
+
+	jmp bad_usage
+help:
+	mov rsi, _help_text
+	mov rdi, STDOUT_FILENO
+	call fprint
+
+	mov rdi, 0
+	jmp exit
 exit:
 	mov rax, 60
 	syscall
-help:
-	mov rsi, help_text
-	mov rdi, STDOUT_FILENO
-	call fprint
-	
-	mov rdi, 0
-	jmp exit
 
 argc rq 1
 argv rq 1
 
-help_short db "-h", 0
-help_long db "--help", 0
-help_text db "usage:", 10,  9, "vie <source> <out>", 10, "options:", 10, 9, "-h --help    show this screen", 10, 0
+_help_short db "-h", 0
+_help_long db "--help", 0
+_help_text db "usage: vie <source> [output]", 10
+           db "options:", 10, 9
+           db "-h --help    show this screen", 10
+           db 0
